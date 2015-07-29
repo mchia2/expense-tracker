f7b512758d2eb06132bccee1cb558fb33d8cd1fa
Matthias Chia <mchia2@gmail.com>
2015-07-29 04:23:40 -0400

    Finish layout and added different routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Expense Tracker App"
+    assert_select "title", "Expense Tracker App"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Expense Tracker App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Expense Tracker App"
   end
 
 end
